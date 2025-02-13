7558b06281ba917d75c7fa6b30ed38a43cb0e36c
你的名字 <>
2025-02-13 16:05:17 -0600

    Initial commit: add yoga_project-8 files

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify
-from datetime import datetime
+
+from flask import Flask, request, jsonify, render_template # 务必确保导入 render_template
 
 app = Flask(__name__)
+
+# 添加 根路径 '/' 的路由，处理函数为 index()，返回 test.html 页面
+@app.route('/')
+def index():
+    return render_template('test.html')
 
 @app.route('/detect_pose', methods=['POST'])
 def detect_pose():
@@ -11,7 +16,7 @@ def detect_pose():
     """
     # 这里可以通过 request.files 获取上传的图像，例如：
     # image_file = request.files.get('image')
-    
+
     # 示例返回数据
     result = {
         "pose": "下犬式",
@@ -23,4 +28,3 @@ def detect_pose():
 if __name__ == '__main__':
     # 让服务器在局域网中可访问，监听 5000 端口
     app.run(host='0.0.0.0', port=5000, debug=True)
-
